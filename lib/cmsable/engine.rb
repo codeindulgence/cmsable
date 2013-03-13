7630b4c70f549e74c7d98fd57ada57086f0f8a06
Nick Butler <nick@hhd.com.au>
2013-03-13 15:39:37 +1100

    Tell host app to load helpers

--- lib/cmsable/engine.rb
+++ lib/cmsable/engine.rb
@@ -1,5 +1,11 @@
 module Cmsable
   class Engine < ::Rails::Engine
     isolate_namespace Cmsable
+    # This makes the consuming app load our helpers
+    initializer 'cmsable.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Cmsable::CmsableHelper
+      end
+    end
   end
 end
